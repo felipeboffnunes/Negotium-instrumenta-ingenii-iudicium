32e919ceabced35f4ca55474c4b885729656f9e0
Felipe Boff Nunes <felipeboffnunes@protonmail.com>
2020-05-29 11:21:55 -0300

    initial commit

---
+++ components/app.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from components.pages import pages
+
+external_stylesheets = [dbc.themes.LUX]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "17rem",
+    "padding": "2rem 1rem",
+    "backgroundColor": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "marginLeft": "17rem"
+}
+
+
+sidebar = html.Div(
+    [
+        html.H2("Censo Brasileiro 2010", className="display-5"),
+        html.Hr(),
+        html.P(
+            "Uma interface para comparação de usabilidade das principais ferramentas de BI no mercado usando o censo brasileiro de 2010.", className="lead", style={'fontSize' : '15px'}
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home",            href="/page-1", id="page-1-link"),
+                dbc.NavLink("Tableau Public",  href="/page-2", id="page-2-link"),
+                dbc.NavLink("Power BI",        href="/page-3", id="page-3-link"),
+                dbc.NavLink("QlikView",        href="/page-4", id="page-4-link"),
+                dbc.NavLink("Python (Altair)", href="/page-5", id="page-5-link"),
+                dbc.NavLink("About",           href="/page-6", id="page-6-link")
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+# this callback uses the current pathname to set the active state of the
+# corresponding nav link to true, allowing users to see page they are on
+@app.callback(
+    [Output(f"page-{i}-link", "active") for i in range(1, 7)],
+    [Input("url", "pathname")],
+)
+def toggle_active_links(pathname):
+    if pathname == "/":
+        # Treat page 1 as the homepage / index
+        return True, False, False
+    return [pathname == f"/page-{i}" for i in range(1, 7)]
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1"]:
+        return pages['tableau']
+    elif pathname == "/page-2":
+        return pages['tableau']
+    elif pathname == "/page-3":
+        return pages['power_bi']
+    elif pathname == "/page-3":
+        return pages['altair']
+    elif pathname == "/page-4":
+        return pages['altair']
+    elif pathname == "/page-5":
+        return pages['altair']
+    elif pathname == "/page-6":
+        return pages['altair']
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
---
+++ components/pages.py
@@ -0,0 +1,105 @@
+import dash_core_components as dcc
+import dash_html_components as html
+
+from components.altair.altair_visualization import altair_visualization
+
+tableau = html.Div([
+            dcc.Tabs([
+                dcc.Tab(
+                    html.Iframe(
+                        id='plot',
+                        sandbox='allow-same-origin allow-scripts',
+                        
+                        src="https://public.tableau.com/views/CensusBrazil2010/Painel1?:display_count=y&publish=yes&:origin=viz_share_link:showVizHome=no&:embed=true",
+            
+                        style={'borderWidth': '0px', 'width' : '100%', 'height' : '85vh', 'padding': '0px'}
+                    )
+                , label="Timeseries Population"),
+                dcc.Tab(
+                    html.Iframe(
+                        id='plot',
+                        sandbox='allow-same-origin allow-scripts',
+
+                        src="https://public.tableau.com/views/CensusBrazil2010-StackedAgePopulation/Painel2?:display_count=y&:origin=viz_share_link:showVizHome=no&:embed=true",
+            
+                        style={'borderWidth': '0px', 'width' : '100%', 'height' : '85vh', 'padding': '0px'}
+                    )
+                , label="Stacked Age Population"),
+                dcc.Tab(
+                    html.Iframe(
+                        id='plot',
+                        sandbox='allow-same-origin allow-scripts',
+
+                        src="https://public.tableau.com/views/CensusBrazil2010-BarsPopulation/Painel3?:display_count=y&publish=yes&:origin=viz_share_link:showVizHome=no&:embed=true",
+            
+                        style={'borderWidth': '0px', 'width' : '100%', 'height' : '85vh', 'padding': '0px'}
+                    )
+                , label="Bars Population"),
+                dcc.Tab(
+                    html.Iframe(
+                        id='plot',
+                        sandbox='allow-same-origin allow-scripts',
+
+                        src="https://public.tableau.com/views/CensusBrazil2010-CirclesResidences/Painel4?:display_count=y&publish=yes&:origin=viz_share_link:showVizHome=no&:embed=true",
+            
+                        style={'borderWidth': '0px', 'width' : '100%', 'height' : '85vh', 'padding': '0px'}
+                    )
+                , label="Circles Residences"),
+                dcc.Tab(
+                    html.Iframe(
+                        id='plot',
+                        sandbox='allow-same-origin allow-scripts',
+
+                        src="https://public.tableau.com/shared/CQ3TSX555?:display_count=y&:origin=viz_share_link:showVizHome=no&:embed=true",
+            
+                        style={'borderWidth': '0px', 'width' : '100%', 'height' : '85vh', 'padding': '0px'}
+                    )
+                , label="Maps Population"),
+                dcc.Tab(
+                    html.Iframe(
+                        id='plot',
+                        sandbox='allow-same-origin allow-scripts',
+
+                        src="https://public.tableau.com/views/CensusBrazil2010-MapsResidences/Painel6?:display_count=y&publish=yes&:origin=viz_share_link:showVizHome=no&:embed=true",
+            
+                        style={'borderWidth': '0px', 'width' : '100%', 'height' : '85vh', 'padding': '0px'}
+                    ), label="Maps Residences"),
+            ])
+        ], style={'height' : '100vh', 'padding': '0px'})
+power_bi = html.Div([
+            dcc.Tabs([
+                dcc.Tab(
+                    html.Iframe(
+                        id='plot',
+                        sandbox='allow-same-origin allow-scripts allow-popups allow-forms allow-cross-origin',
+
+                        src="https://app.powerbi.com/reportEmbed?reportId=1bc58b44-49a5-4986-902b-45de42f4d5bb&autoAuth=true&ctid=51ebcf31-5839-412e-83bb-801a2ba78627&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D",
+                    
+                        style={'borderWidth': '0px', 'width' : '100%', 'height' : '85vh', 'padding': '0px'}
+                    ), label="População por Ano e Estado"),
+                dcc.Tab(
+                    html.Iframe(
+                        id='plot',
+                        sandbox='allow-same-origin allow-scripts allow-popups allow-forms allow-cross-origin',
+
+                        src="https://app.powerbi.com/reportEmbed?reportId=350246cf-b777-4a83-9b67-6b2503a8424c&autoAuth=true&ctid=51ebcf31-5839-412e-83bb-801a2ba78627&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D",
+                    
+                        style={'borderWidth': '0px', 'width' : '100%', 'height' : '85vh', 'padding': '0px'}
+                    ), label="Domicílios por Estado e Categoria")
+            ])
+        ], style={'height' : '100vh', 'padding': '0px'})
+
+altair = html.Div([
+            html.Iframe(
+                id='plot-altair',
+                sandbox='allow-scripts',
+
+                srcDoc=altair_visualization,
+
+                style={'borderWidth': '0px', 'width' : '100%', 'height' : '100vh', 'padding': '0px'}
+            )     
+        ], style={'height' : '100vh', 'padding': '0px'})
+
+pages = {'altair' : altair,
+         'tableau' : tableau,
+         'power_bi' : power_bi}
